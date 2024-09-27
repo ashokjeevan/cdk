8dfd79fb7f6e8afb2425294bd7c6f0421ec832df
Ashok <ashokjeevan@gmail.com>
2024-09-26 23:32:22 -0400

    api gateway with lambda association

--- cdk-infra/cdk_infra/cdk_infra_stack.py
+++ cdk-infra/cdk_infra/cdk_infra_stack.py
@@ -1,7 +1,9 @@
 from aws_cdk import (
   Stack,
   aws_lambda as _lambda, # Import the Lambda module
-  CfnOutput
+  CfnOutput,
+  # Import API Gateway L2 construct
+    aws_apigateway as apigateway
 )
 from constructs import Construct
 
@@ -10,27 +12,47 @@ class CdkInfraStack(Stack):
   def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
     super().__init__(scope, construct_id, **kwargs)
 
-    # Define the Lambda function resource
-    my_function = _lambda.Function(
-      self, "HelloWorldFunction", 
-      runtime = _lambda.Runtime.NODEJS_20_X, # Provide any supported Node.js runtime
-      handler = "index.handler",
-      code = _lambda.Code.from_inline(
-        """
-        exports.handler = async function(event) {
-          return {
-            statusCode: 200,
-            body: JSON.stringify('You are now seeing this code'),
-          };
-        };
-        """
-      ),
+    hello_world_function = _lambda.Function(
+            self,
+            "HelloWorldFunction",
+            runtime = _lambda.Runtime.NODEJS_20_X, # Choose any supported Node.js runtime
+            code = _lambda.Code.from_asset("lambda"), # Points to the lambda directory
+            handler = "hello.handler", # Points to the 'hello' file in the lambda directory
+        )
+    
+    # Define the API Gateway resource
+    api = apigateway.LambdaRestApi(
+        self,
+        "HelloWorldApi",
+        handler = hello_world_function,
+        proxy = False,
     )
+    
+    # Define the '/hello' resource with a GET method
+    hello_resource = api.root.add_resource("hello")
+    hello_resource.add_method("GET")
+    
+    # # Define the Lambda function resource
+    # my_function = _lambda.Function(
+    #   self, "HelloWorldFunction", 
+    #   runtime = _lambda.Runtime.NODEJS_20_X, # Provide any supported Node.js runtime
+    #   handler = "index.handler",
+    #   code = _lambda.Code.from_inline(
+    #     """
+    #     exports.handler = async function(event) {
+    #       return {
+    #         statusCode: 200,
+    #         body: JSON.stringify('You are now seeing this code'),
+    #       };
+    #     };
+    #     """
+    #   ),
+    # )
 
-    # Define the Lambda function URL resource
-    my_function_url = my_function.add_function_url(
-      auth_type = _lambda.FunctionUrlAuthType.NONE,
-    )
+    # # Define the Lambda function URL resource
+    # my_function_url = my_function.add_function_url(
+    #   auth_type = _lambda.FunctionUrlAuthType.NONE,
+    # )
 
-    # Define a CloudFormation output for your URL
-    CfnOutput(self, "myFunctionUrlOutput", value=my_function_url.url)
+    # # Define a CloudFormation output for your URL
+    # CfnOutput(self, "myFunctionUrlOutput", value=my_function_url.url)
